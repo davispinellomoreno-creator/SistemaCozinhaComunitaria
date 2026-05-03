986d87758aa9b0d7406a01582d1d9ddba3b7c3cd
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-03 19:21:49 -0300

    primeiro commit

---
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/SistemaCozinhaComunitariaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SistemaCozinhaComunitaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaCozinhaComunitariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaCozinhaComunitariaApplication.class, args);
+	}
+
+}
---
+++ SistemaCozinhaComunitaria/src/test/java/com/example/SistemaCozinhaComunitaria/SistemaCozinhaComunitariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SistemaCozinhaComunitaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCozinhaComunitariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
