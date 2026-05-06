d68ed261d403e761ec1defa7802f5a705b9a3600
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-06 16:06:55 -0300

    Implementação do model

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Entity/Produtos.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Entity/Produtos.java
@@ -1,7 +1,24 @@
 package com.example.SistemaCozinhaComunitaria.Entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
 
 @Entity
+@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table( name = "Cadastro_Produtos")
 public class Produtos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String produto;
+    private LocalDate validade;
+    private double quantidade;
 }
