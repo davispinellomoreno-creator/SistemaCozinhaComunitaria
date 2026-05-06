900725e1357459d04d1e07c4ecfb9e8b28cc9970
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-06 16:09:01 -0300

    Implementando o DTO

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Dto/ProdutoDto.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Dto/ProdutoDto.java
@@ -1,4 +1,16 @@
 package com.example.SistemaCozinhaComunitaria.Dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class ProdutoDto {
+
+    private Long id;
+    private String produto;
+    private LocalDate validade;
+    private double quantidade;
 }
