e1db1334ea73a614632f83242be9618af731a2bb
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-07 10:56:15 -0300

    Apenas um commit para salavr uma pequena alteração

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/ProdutosService.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/ProdutosService.java
@@ -1,5 +1,7 @@
 package com.example.SistemaCozinhaComunitaria.Service;
 
+import com.example.SistemaCozinhaComunitaria.Dto.ProdutoDto;
+import com.example.SistemaCozinhaComunitaria.Entity.Produtos;
 import com.example.SistemaCozinhaComunitaria.Repository.ProdutosRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +10,20 @@ public class ProdutosService {
 
     private final ProdutosRepository repository;
 
-
-
     public ProdutosService(ProdutosRepository repository) {
         this.repository = repository;
     }
 
+    public void SalvarProduto(ProdutoDto produto){
+
+
+
+
+
+
+
+
+
+    }
 
 }
