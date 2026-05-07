dfa94829e87c39ba2084f2b32c3ad93bc7fd3ee4
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-07 10:47:34 -0300

    Começando a logica do Service

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/ProdutosService.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/ProdutosService.java
@@ -1,4 +1,18 @@
 package com.example.SistemaCozinhaComunitaria.Service;
 
+import com.example.SistemaCozinhaComunitaria.Repository.ProdutosRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutosService {
+
+    private final ProdutosRepository repository;
+
+
+
+    public ProdutosService(ProdutosRepository repository) {
+        this.repository = repository;
+    }
+
+
 }
