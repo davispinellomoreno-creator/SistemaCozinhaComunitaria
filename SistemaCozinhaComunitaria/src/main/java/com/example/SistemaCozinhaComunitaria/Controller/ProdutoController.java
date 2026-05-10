7e25c87003088b3d689dce180063c5e59473c27c
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-09 21:13:11 -0300

    Getmapping criado

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Controller/ProdutoController.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Controller/ProdutoController.java
@@ -1,9 +1,27 @@
 package com.example.SistemaCozinhaComunitaria.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.SistemaCozinhaComunitaria.Entity.Produtos;
+import com.example.SistemaCozinhaComunitaria.Service.ProdutosService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/Produtos")
 public class ProdutoController {
+
+
+    private final ProdutosService produtoservice;
+
+    public ProdutoController(ProdutosService produtoservice) {
+        this.produtoservice = produtoservice;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produtos> buscarProduto(@PathVariable UUID id) {
+        return ResponseEntity.ok(produtoservice.buscarProdutoPorId(id));
+    }
+
+
 }
