82da1bd44d62d70e2c6f0677a311cf79a1c78e02
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-15 23:07:44 -0300

    Implementando regra de negocio em classes criadas recentemente

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Entity/Alimentacao.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Entity/Alimentacao.java
@@ -1,4 +1,26 @@
 package com.example.SistemaCozinhaComunitaria.Entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table( name = "Cadastro_Alimentacao")
 public class Alimentacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String alimentacao;
+
+
+
 }
--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Entity/Usuario.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Entity/Usuario.java
@@ -1,9 +1,18 @@
 package com.example.SistemaCozinhaComunitaria.Entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.UUID;
 
+@Entity
+@Data
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table( name = "Cadastro_Usuario")
 public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
