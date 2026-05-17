eee9eedc7104d010ce7bad77a4c0a09ae83a3237
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-16 22:25:51 -0300

    Implementação das anotações e do repository no Service

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/AlimentacaoService.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/AlimentacaoService.java
@@ -1,4 +1,15 @@
 package com.example.SistemaCozinhaComunitaria.Service;
 
+import com.example.SistemaCozinhaComunitaria.Repository.AlimentacaoRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AlimentacaoService {
+
+    private final AlimentacaoRepository alimentacaoRepository;
+
+    public AlimentacaoService(AlimentacaoRepository alimentacaoRepository){
+        this.alimentacaoRepository = alimentacaoRepository;
+    }
+
 }
--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/UsuarioService.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/UsuarioService.java
@@ -1,4 +1,17 @@
 package com.example.SistemaCozinhaComunitaria.Service;
 
+import com.example.SistemaCozinhaComunitaria.Repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
+
+
 }
