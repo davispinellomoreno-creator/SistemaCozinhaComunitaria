cad96e5c2eaf87f0be1925e15d42e54dcd7d84fd
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-17 21:12:17 -0300

    Tentando corrigir erros, mas amanhã irei corrigir o restante dos erros

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Entity/Produtos.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Entity/Produtos.java
@@ -24,4 +24,5 @@ public class Produtos {
     private double quantidade;
 
 
+
 }
--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/AlimentacaoService.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Service/AlimentacaoService.java
@@ -1,10 +1,11 @@
 package com.example.SistemaCozinhaComunitaria.Service;
 
 import com.example.SistemaCozinhaComunitaria.Dto.AlimentacaoDto;
+import com.example.SistemaCozinhaComunitaria.Dto.ProdutoDto;
 import com.example.SistemaCozinhaComunitaria.Entity.Alimentacao;
 import com.example.SistemaCozinhaComunitaria.Entity.Produtos;
 import com.example.SistemaCozinhaComunitaria.Repository.AlimentacaoRepository;
-import org.antlr.v4.runtime.misc.NotNull;
+
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -14,9 +15,23 @@ public class AlimentacaoService {
 
     private final AlimentacaoRepository alimentacaoRepository;
 
-    public AlimentacaoService(AlimentacaoRepository alimentacaoRepository){
+    public AlimentacaoService(AlimentacaoRepository alimentacaoRepository) {
         this.alimentacaoRepository = alimentacaoRepository;
     }
 
+    public UUID save(AlimentacaoDto dto) {
 
-}
+        Alimentacao entity = new Alimentacao(
+                UUID.randomUUID(),
+                dto.alimentacao()
+        );
+
+        Alimentacao saved = alimentacaoRepository.save(entity);
+
+        return saved.getId();
+    }
+
+
+
+
+
