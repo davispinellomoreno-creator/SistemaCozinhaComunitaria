0394406e910686e657a27c14a4bb2ab0dd4e408d
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-21 23:47:40 -0300

    Resolvendo um erro na alimentaçãoDto

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Dto/AlimentacaoDto.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Dto/AlimentacaoDto.java
@@ -1,8 +1,5 @@
 package com.example.SistemaCozinhaComunitaria.Dto;
 
-import java.util.UUID;
-
 public record AlimentacaoDto(java.util.UUID id, String alimentacao) {
-    public AlimentacaoDto(UUID id, String nome, String email, String senha, Boolean ativo) {
-    }
+
 }
