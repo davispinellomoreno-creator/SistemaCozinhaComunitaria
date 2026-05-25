4d9e28a7b5be163dc7bde3ea085618b622ce0ed6
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-05-24 21:05:45 -0300

    Excluindo um erro das classes dos pacotes Security

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Security/CustomUserDetailsService.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/Security/CustomUserDetailsService.java
@@ -4,6 +4,6 @@ package com.example.SistemaCozinhaComunitaria.Security;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+public class CustomUserDetailsService {
 
 }
