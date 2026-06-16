5ee8547711e688c11c6d3cf69b090b4c00585856
davispinellomoreno-creator <davispinellomoreno@gmail.com>
2026-06-16 10:07:25 -0300

    Problema solucionado!!!!

--- SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/SistemaCozinhaComunitariaApplication.java
+++ SistemaCozinhaComunitaria/src/main/java/com/example/SistemaCozinhaComunitaria/SistemaCozinhaComunitariaApplication.java
@@ -3,6 +3,7 @@ package com.example.SistemaCozinhaComunitaria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class SistemaCozinhaComunitariaApplication {
 
 	public static void main(String[] args) {
